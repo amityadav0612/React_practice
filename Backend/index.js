e21e9f7f479ebe4c603f736cdc81b350ce62c445
amityadav0612 <amity7205@gmail.com>
2025-07-26 19:03:14 +0530

    added all route

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,17 @@ app.use(express.json());
 const {createTodo,updateTodo}=require("./types");
 
 
+
 app.post("/todo",(req,res)=>{
+    const parsedData=createTodo.safeParse(req.body);
+
+    if(!parsedData.success){
+        res.status(400).json({
+            msg:"You send wrong inputs"
+        })
+        return ;
+    }
+    // put it in mongodb
 
 })
 app.get("/todos",(req,res)=>{
@@ -15,5 +25,14 @@ app.get("/todos",(req,res)=>{
     })
 })
 app.put("/completed",(req,res)=>{
+    const parsedData=updateTodo.safeParse(req.body);
+
+    if(!parsedData.success){
+          res.status(400).json({
+            msg:"You have entered wrong todo.."
+        })
+      return ;
+    }
+    //db logic
 
 })
